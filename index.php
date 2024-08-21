3e69adac114f2d1ff57401dd6eff24f8c389f89d
Oglere <davigaru0@gmail.com>
2024-08-21 10:11:32 +0800

    Cataloging | Gayle | Aug 20

--- index.php
+++ index.php
@@ -7,52 +7,29 @@
     <title>DARA Main Page</title>
 </head>
     <body>
-
-        <div class="hamburgermodal">
-            <a href="#">
-                <div class="loginbutton">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="feather feather-log-in"
-                        >
-                        <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-                        <polyline points="10 17 15 12 10 7" />
-                        <line x1="15" y1="12" x2="3" y2="12" />
-                    </svg>
-                    <h4> &nbsp Login</h4>
-                </div>
-            </a>
-        </div>
-
-
         <main>
             <header>
-                <div class="hamburger">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="feather feather-menu"
-                        >
-                        <line x1="3" y1="12" x2="21" y2="12" />
-                        <line x1="3" y1="6" x2="21" y2="6" />
-                        <line x1="3" y1="18" x2="21" y2="18" />
-                    </svg>
-                </div>
+                <a href="#">
+                    <div class="loginbutton">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-log-in"
+                            >
+                            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+                            <polyline points="10 17 15 12 10 7" />
+                            <line x1="15" y1="12" x2="3" y2="12" />
+                        </svg>
+                        <h4> &nbsp Login</h4>
+                    </div>
+                </a>
             </header>
             
             <div class="contents">
@@ -78,6 +55,30 @@
                             <line x1="21" y1="21" x2="16.65" y2="16.65" />
                         </svg>
                     </button>
+                </div>
+
+                <div class="tags">
+                    <div class="tag">
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-tag"
+                            >
+                            <path
+                                d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"
+                            />
+                            <line x1="7" y1="7" x2="7.01" y2="7" />
+                        </svg>
+                        &nbsp;Catalogs
+                    </div>
+                <div class="tagform">asd</div>
                 </div>
             </div>
 
