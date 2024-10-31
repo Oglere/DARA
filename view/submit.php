6d886a51cd07f84bff2e89048664f6f7889c75b6
Oglere <gambaza66@gmail.com>
2024-10-31 09:57:27 +0800

    New UI | Oct 31

--- view/submit.php
+++ view/submit.php
@@ -87,9 +87,70 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 </div>
 
                 <nav class="nav-links">
-                    <a href="student_dashboard.php">Dashboard</a>
-                    <a href="submit.php">Submit Studies</a>
-                    <a href="status.php">View Study Status</a>
+                    <a href="student_dashboard.php"> 
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-home"
+                            >
+                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                            <polyline points="9 22 9 12 15 12 15 22" />
+                        </svg>
+
+                        Dashboard
+                    </a>
+                    <a href="submit.php">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-file-plus"
+                            >
+                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+                            <polyline points="14 2 14 8 20 8" />
+                            <line x1="12" y1="18" x2="12" y2="12" />
+                            <line x1="9" y1="15" x2="15" y2="15" />
+                        </svg>
+                    
+                        Submit Studies
+                    </a>
+                    <a href="status.php">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-eye"
+                            >
+                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                            <circle cx="12" cy="12" r="3" />
+                        </svg>
+
+                        View Study Status
+                    </a>
+
+                    <div class="asd2" style=" width: 100%; margin-top: 10px; display: flex; justify-content: center;">
+                        <div class="asd3" style="border-bottom: 1px solid black; width: 150px;"></div>
+                    </div>
+
                     <a href="../">Search Studies</a>
                 </nav>
             </div>
